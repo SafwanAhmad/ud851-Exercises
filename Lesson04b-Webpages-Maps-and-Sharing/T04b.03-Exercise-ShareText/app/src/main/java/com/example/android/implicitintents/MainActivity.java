08cb0b926905a5d2e7afc176254d4a596b2c742d
Safwan Ahmad <safwan@localhost.localdomain>
2018-04-15 19:52:57 +0530

    Completed Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.implicitintents;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -70,6 +71,8 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO (6) Replace the Toast with shareText, passing in the String from step 5
         Toast.makeText(this, "TODO: Share text when this is clicked", Toast.LENGTH_LONG).show();
+        String toShare = "The quick brown for jumps over the lazy dog.";
+        shareText(toShare);
     }
 
     /**
@@ -151,4 +154,16 @@ public class MainActivity extends AppCompatActivity {
         // TODO (3) Create a title for the chooser window that will pop up
 
         // TODO (4) Use ShareCompat.IntentBuilder to build the Intent and start the chooser
+
+    private void shareText(String text) {
+        String mimeType = "text/plain";
+        String title = "Share Intent";
+        ShareCompat.IntentBuilder shareIntent = ShareCompat.IntentBuilder.from(this)
+                .setType(mimeType)
+                .setChooserTitle(title)
+                .setText(text);
+        if (shareIntent.getIntent().resolveActivity(getPackageManager()) != null) {
+            startActivity(shareIntent.getIntent());
+        }
+    }
 }
